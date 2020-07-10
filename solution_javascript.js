a757c2976b77f5c8cfb0428fe16e5b2b84b55028
Luca Alfieri <lalfieri@uoguelph.ca>
2020-07-10 14:24:47 -0400

    student fall 2020 Luca Alfieri

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,59 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this. cur=-1;
+    this.actions=[];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+
+  add(num) {
+  	let temp = this.value;
+  	temp+=num;
+  	this.actions.push(num);
+  	this.value=temp;
+  	this.cur++;
+  }
+
+  subtract(num) {
+  	let temp= this.value;
+  	temp-=num;
+  	this.actions.push(num*-1);
+  	this.value=temp;
+  	this.cur++;
+  }
+
+  undo() {
+  	if(this.cur>0){
+  		this.value -= this.actions[this.cur];
+  		this.cur--;
+  	}
+  	else{
+  		this.cur=-1;
+  		this.value=0;
+  	}
+  }
+
+  redo() {
+  	if(this.actions.length-1>this.cur){
+  		this.cur++;
+  		this.value += this.actions[this.cur];
+  	}
+  }
+
+  bulk_undo(num) {
+  	let i;
+  	for(i=0;i<num;i++){
+  		this.undo();
+  	}
+  }
+
+  bulk_redo(num) {
+  	let i;
+  	for(i=0;i<num;i++){
+  		this.redo();
+  	}
+  }
+
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
